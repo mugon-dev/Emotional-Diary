e7650aa4f37f70cbf51a3e03b3a81b2ea852fe77
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2020-12-07 13:56:09 +0900

    django react 연결

--- emotion_diary/emotion_diary/urls.py
+++ emotion_diary/emotion_diary/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('member/', include('member.urls')),
     path('board/', include('board.urls')),
+    path('', include('frontend.urls')),
 ]
