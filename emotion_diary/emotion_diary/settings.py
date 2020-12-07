c587a45e0b04d4a23d6ab10b872ad8df12e7dc1f
sumi <suuumi08@gmail.com>
2020-12-07 17:01:46 +0900

    회원가입, 로그인

--- emotion_diary/emotion_diary/settings.py
+++ emotion_diary/emotion_diary/settings.py
@@ -126,7 +126,5 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
 }
