f679d8a3bca3a02419c5f29b5640856f08aff783
sumi <suuumi08@gmail.com>
2020-12-08 10:08:21 +0900

    url 버그수정

--- emotion_diary/member/urls.py
+++ emotion_diary/member/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
 from .views import main, RegistrationAPI, LoginAPI, UserAPI
+from knox import views as knox_views
 
 urlpatterns = [
     path('', main),
     path("auth/register/", RegistrationAPI.as_view()),
     path("auth/login/", LoginAPI.as_view()),
     path("auth/user/", UserAPI.as_view()),
+    path('api/auth/logout', knox_views.LogoutView.as_view(), name='knox_logout')
+
 ]
