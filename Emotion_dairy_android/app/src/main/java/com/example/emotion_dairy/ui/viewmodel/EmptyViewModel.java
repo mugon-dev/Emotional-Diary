e3f5e2d5ac060a738d4a11e7e184d568dfacf54d
dongwoo4791 <69226747+dongwoo4791@users.noreply.github.com>
2020-12-09 14:22:39 +0900

    4. 월 달력 기초

--- Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/ui/viewmodel/EmptyViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.example.emotion_dairy.ui.viewmodel;
-
-import androidx.lifecycle.ViewModel;
-
-public class EmptyViewModel extends ViewModel {
-}
--- Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/utils/Keys.java
+++
@@ -1,6 +0,0 @@
-package com.example.emotion_dairy.utils;
-
-public class Keys {
-    public static final String EMPTY = "empty";
-}
-
