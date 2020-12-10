44959eac99260f4f01d71a00b27b38ab25e1a839
dongwoo4791 <69226747+dongwoo4791@users.noreply.github.com>
2020-12-10 17:55:48 +0900

    8. 주별 달력 안그려짐

--- Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/ui/home/HomeFragment.java
+++ Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import android.widget.Button;
 
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,14 +22,21 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.annimon.stream.Stream;
 import com.applandeo.materialcalendarview.CalendarView;
 import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.example.emotion_dairy.R;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import static android.content.Context.MODE_NO_LOCALIZED_COLLATORS;
@@ -52,9 +61,39 @@ public class HomeFragment extends Fragment {
 
         CalendarView calendarView = (CalendarView) root.findViewById(R.id.calendarView);
         calendarView.setEvents(events);
+
+
+
+
         //추가 끝
 
+        TextView tv_date;
+        tv_date=(TextView)root.findViewById(R.id.tv_date);
+        calendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                Calendar clickedDayCalendar = eventDay.getCalendar();
+                Calendar selectedDate = calendarView.getFirstSelectedDate();
+                Log.d("log","날짜 찍음");
+        //        Log.d("log", Integer.toString(clickedDayCalendar.getWeekYear()));
+          //      Log.d("log", Integer.toString(clickedDayCalendar.get));
+                Log.d("log", clickedDayCalendar.getTime().toString());
+              //  Log.d("log",)
+              //tv_date.setText(clickedDayCalendar.getTime().toString().substring(0,10));
+
+                //선택날짜 출력하기
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+              String strDate = simpleDateFormat.format(clickedDayCalendar.getTime());
+                tv_date.setText(strDate);
+
+            }
+        });
+
+
         return root;
+
     }
+
+
 }
 
