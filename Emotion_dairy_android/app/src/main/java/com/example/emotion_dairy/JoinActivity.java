74fe99e1c4ed921e0f47189c817e6bfbd7e356c2
dongwoo4791 <69226747+dongwoo4791@users.noreply.github.com>
2020-12-10 11:41:30 +0900

    7.회원가입 1차디자인

---
+++ Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/JoinActivity.java
@@ -0,0 +1,13 @@
+package com.example.emotion_dairy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class JoinActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.join);
+    }
+}
--- Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/LoginActivity.java
+++ Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/LoginActivity.java
@@ -20,6 +20,13 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
      //   finish(); //뒤로가기 눌러도 메인으로 로그인화면으로 안넘어감
     }
-
+    //회원가입 버튼 이벤트
+    public void btn_join(View view){
+        Intent intent = new Intent(this,JoinActivity.class);
+        startActivity(intent);
+        //   finish(); //뒤로가기 눌러도 메인으로 로그인화면으로 안넘어감
     }
+
+
+}
 
