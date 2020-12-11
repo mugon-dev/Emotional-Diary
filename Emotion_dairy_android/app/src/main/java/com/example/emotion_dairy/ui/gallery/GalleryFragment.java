f737906811704c16e5d442703fc09ceae268befb
dongwoo4791 <69226747+dongwoo4791@users.noreply.github.com>
2020-12-11 17:54:44 +0900

    12. 마무리

--- Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/ui/gallery/GalleryFragment.java
+++ Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/ui/gallery/GalleryFragment.java
@@ -1,23 +1,33 @@
 package com.example.emotion_dairy.ui.gallery;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.emotion_dairy.ListViewAdapter;
+import com.example.emotion_dairy.ListViewItem;
 import com.example.emotion_dairy.R;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
+import java.util.ArrayList;
+
 public class GalleryFragment extends Fragment {
+
+    private ListView listview;
+    private ListViewAdapter adapter;
 
     private GalleryViewModel galleryViewModel;
 
@@ -31,21 +41,48 @@ public class GalleryFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
 
 
-        mvc=root.findViewById(R.id.calendarView3);
+        mvc = root.findViewById(R.id.calendarView3);
         mvc.state().edit().isCacheCalendarPositionEnabled(false)
                 .setFirstDayOfWeek(1)
-                .setMinimumDate(CalendarDay.from(2018,1,1))
-                .setMaximumDate(CalendarDay.from(2020,12,31))
+                .setMinimumDate(CalendarDay.from(2018, 1, 1))
+                .setMaximumDate(CalendarDay.from(2020, 12, 31))
                 .setCalendarDisplayMode(CalendarMode.WEEKS)
                 .commit();
 
-   //     final TextView textView = root.findViewById(R.id.text_gallery);
-    //    galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-      //      @Override
+        //     final TextView textView = root.findViewById(R.id.text_gallery);
+        //    galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //      @Override
         //    public void onChanged(@Nullable String s) {
-          //      textView.setText(s);
-         //   }
-       // });
+        //      textView.setText(s);
+        //   }
+        // });
+
+
+// Adapter 생성
+//        adapter = new ListViewAdapter();
+//
+//        // 리스트뷰 객체 생성 및 Adapter 설정
+//        listview = (ListView) root.findViewById(R.id.listView);
+//        listview.setAdapter(adapter);
+//
+//        // 리스트 뷰 아이템 추가.
+//        adapter.addItem("테스트제목1","테스트내용1","테스트이름1","테스트날짜1");
+//        adapter.addItem("테스트제목2","테스트내용2","테스트이름2","테스트날짜2");
+//        adapter.addItem("테스트제목3","테스트내용3","테스트이름3","테스트날짜3");
+//        adapter.addItem("테스트제목3","테스트내용3","테스트이름3","테스트날짜3");
+
+//        ArrayList<ListViewItem> listViewItems= new ArrayList<ListViewItem>();
+//
+//        for(int i=1;i<5;i++){
+//            ListViewItem list = new ListViewItem("date"+i,"name"+i,"title"+i,"content"+i);
+//            listViewItems.add(list);
+//        }
+//        adapter = new ListViewAdapter(listViewItems);
+//        Log.d("log",listViewItems.get(1).toString());
+
+
+    final ListViewAdapter myAdapter = new ListViewAdapter(this,);
+
         return root;
     }
 }
