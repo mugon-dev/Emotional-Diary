566d9598a39297e92a1e3db6fe1a73e5befa87ca
dongwoo4791 <69226747+dongwoo4791@users.noreply.github.com>
2020-12-14 17:03:33 +0900

    19.그룹 리스트 뿌리기

--- Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/ui/gallery/GalleryFragment.java
+++ Emotion_dairy_android/app/src/main/java/com/example/emotion_dairy/ui/gallery/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.example.emotion_dairy.ui.gallery;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,6 +19,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
 import com.example.emotion_dairy.GroupList;
 import com.example.emotion_dairy.ListViewAdapter;
 import com.example.emotion_dairy.ListViewItem;
@@ -33,13 +36,16 @@ import com.google.gson.reflect.TypeToken;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -50,6 +56,7 @@ import retrofit2.Response;
 
 public class GalleryFragment extends Fragment {
 
+    ArrayList<SoloBoardDTO> soloBoardDTOList = new ArrayList<SoloBoardDTO>();
     private ListView listview;
     private ListViewAdapter adapter;
     List<GroupList> list = new ArrayList<GroupList>();
@@ -70,6 +77,11 @@ public class GalleryFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
 
         api= HttpClient.getRetrofit().create(ApiInterface.class);
+        // Adapter 생성
+
+
+
+
         mvc = root.findViewById(R.id.calendarView3);
         mvc.state().edit().isCacheCalendarPositionEnabled(false)
                 .setFirstDayOfWeek(1)
@@ -77,6 +89,39 @@ public class GalleryFragment extends Fragment {
                 .setMaximumDate(CalendarDay.from(2020, 12, 31))
                 .setCalendarDisplayMode(CalendarMode.WEEKS)
                 .commit();
+
+        //클릭이벤트
+mvc.setOnDateChangedListener(new OnDateSelectedListener() {
+    @Override
+    public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+
+        adapter = new ListViewAdapter();
+
+        // 리스트뷰 객체 생성 및 Adapter 설정
+        listview = (ListView) root.findViewById(R.id.listView);
+        listview.setAdapter(adapter);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String strDate = simpleDateFormat.format(date.getCalendar().getTime());
+        Log.d("log",strDate);
+
+
+
+        for (int i = 0; i < soloBoardDTOList.size(); i++) {
+            if (strDate.equals(soloBoardDTOList.get(i).getCreateTime())) {
+                Log.d("log","if문 들어옴");
+                Log.d("log",soloBoardDTOList.get(i).toString());
+                Log.d("log",soloBoardDTOList.get(i).getTitle());
+                Log.d("log",soloBoardDTOList.get(i).getMember().getName());
+                Log.d("log",soloBoardDTOList.get(i).getCreateTime());
+
+                adapter.addItem(soloBoardDTOList.get(i).getTitle(),soloBoardDTOList.get(i).getMember().getName(),soloBoardDTOList.get(i).getEmotion());
+            }
+        }
+
+    }
+});
+
 
         //그룹작업
         spinnerGroup=root.findViewById(R.id.spinnerGroup);
@@ -97,12 +142,33 @@ public class GalleryFragment extends Fragment {
 
         spinnerGroup.setAdapter(spinnerAdapter);
         spinnerGroup.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 tvGroupName.setText(list.get(i).getTname());
                 Log.d("log","그룹 선택 : " + list.get(i).getTno()+" 선택됨");
                 //그룹 선택시 그룹 리스트 뽑아옴(SharedPreference or retrofit2)
+                String auth = PreferenceManager.getString(getContext(),"Auth");
+                Call<ArrayList<SoloBoardDTO>> call = api.getGroupBoard(auth,list.get(i).getTno());
+
+                call.enqueue(new Callback<ArrayList<SoloBoardDTO>>() {
+                    @Override
+                    public void onResponse(Call<ArrayList<SoloBoardDTO>> call, Response<ArrayList<SoloBoardDTO>> response) {
+                        Log.d("log","그룹 게시물 데이터 통신 성공");
+
+                        soloBoardDTOList=response.body();
+                        Log.d("log",soloBoardDTOList.toString());
+                    }
+
+                    @Override
+                    public void onFailure(Call<ArrayList<SoloBoardDTO>> call, Throwable t) {
+                        Log.d("log","그룹 게시물 데이터 통신 실패");
+                    }
+                });
+
             }
+
+
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
@@ -110,18 +176,8 @@ public class GalleryFragment extends Fragment {
             }
         });
 
-// Adapter 생성
-        adapter = new ListViewAdapter();
 
-        // 리스트뷰 객체 생성 및 Adapter 설정
-        listview = (ListView) root.findViewById(R.id.listView);
-        listview.setAdapter(adapter);
 
-        // 리스트 뷰 아이템 추가.
-        adapter.addItem("테스트제목1","테스트내용1","테스트이름1","테스트날짜1");
-        adapter.addItem("테스트제목2","테스트내용2","테스트이름2","테스트날짜2");
-        adapter.addItem("테스트제목3","테스트내용3","테스트이름3","테스트날짜3");
-        adapter.addItem("테스트제목3","테스트내용3","테스트이름3","테스트날짜3");
 
 
 
